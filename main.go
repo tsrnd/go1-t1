aa34d4cdb3b405cb7d273f911fe8cfabd1b6a396
at-dieule <dieu.le@asiantech.vn>
2017-12-14 14:48:23 +0700

    new project arch

--- main.go
+++ main.go
@@ -1,27 +1,18 @@
-
 package main
 
 import (
-    "net/http"
-    "goweb1/router"
-  _ "github.com/go-sql-driver/mysql"
-    "goweb1/database"
-    "goweb1/model"
-    "goweb1/config"
-    "os"
-    "github.com/gorilla/csrf"
+	"log"
+	"net/http"
+
+	"github.com/tsrnd/go-clean-arch/config"
 )
 
-
 func main() {
-    r := router.Routes()
-    http.ListenAndServe(os.Getenv("SERVER_PORT"), csrf.Protect([]byte("32-byte-long-auth-key"), csrf.Secure(false))(r))
+	db := config.DB()
+	cache := config.Cache()
+	router := config.Router(db, cache)
+	port := config.Port()
+	if err := http.ListenAndServe(p, r); err != nil {
+		log.Fatal(err)
+	}
 }
-
-func init() {
-    config.SetupEnv()
-    db := database.ConnectDB()
-    model.SetDatabase(db)  
-    http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-}
-
