56cde23cbc6a69a237e7a064f1655cbf1753e28a
Loc Tang V <loc.tang@asiantech.vn>
2017-12-18 14:08:56 +0700

    mock repo GetById

--- user/delivery/http/user_controller_test.go
+++ user/delivery/http/user_controller_test.go
@@ -1,1 +1,32 @@
 package http_test
+
+import (
+	"goweb1/user/usecase"
+	"net/http"
+	"testing"
+)
+
+func TestUserController_UserLogin(t *testing.T) {
+	type fields struct {
+		Usecase usecase.UserUsecase
+	}
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := &UserController{
+				Usecase: tt.fields.Usecase,
+			}
+			ctrl.UserLogin(tt.args.w, tt.args.r)
+		})
+	}
+}
