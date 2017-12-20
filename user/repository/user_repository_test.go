df216e4e96c075f655772584d0c83b6fd4708bc7
at-dieule <dieu.le@asiantech.vn>
2017-12-20 09:19:38 +0700

    test get user by email

--- user/repository/user_repository_test.go
+++ user/repository/user_repository_test.go
@@ -40,3 +40,26 @@ func TestGetByID(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, user)
 }
+
+func TestGetByEmail(t *testing.T) {
+	userFake := FakeUserStruct{}
+	err := faker.FakeData(&userFake)
+	if err != nil {
+		fmt.Println(err)
+	}
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	rows := sqlmock.NewRows([]string{
+		"id", "email", "name"}).
+		AddRow(1, "abc@gmail.com", "dieu")
+	query := "select id, email, name from users where email = ?"
+	mock.ExpectQuery(query).WithArgs("abc@gmail.com").WillReturnRows(rows)
+	repo := userRepo.NewUserRepository(db)
+	email := "abc@gmail.com"
+	user, err := repo.GetByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, user)
+}
